c0b24c7d50a40b5bf1f3e6c75e16371a72242606
Chris Xu <chris.xu.mps1@gmail.com>
2025-04-17 01:00:14 -0700

    Wezterm Command+Arrow config support

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,5 @@
 local wezterm = require("wezterm")
-
 CONFIG = wezterm.config_builder()
-
 CONFIG = {
 	automatically_reload_config = true,
 	enable_tab_bar = false,
@@ -36,7 +34,42 @@ CONFIG = {
 			mods = "CMD|SHIFT",
 			action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
 		},
+		-- Command+Left Arrow - move backward one word
+		{
+			key = "LeftArrow",
+			mods = "CMD",
+			action = wezterm.action.SendKey({
+				key = "b",
+				mods = "ALT",
+			}),
+		},
+		-- Command+Right Arrow - move forward one word
+		{
+			key = "RightArrow",
+			mods = "CMD",
+			action = wezterm.action.SendKey({
+				key = "f",
+				mods = "ALT",
+			}),
+		},
+		-- Command+Up Arrow - move to beginning of line
+		{
+			key = "UpArrow",
+			mods = "CMD",
+			action = wezterm.action.SendKey({
+				key = "a",
+				mods = "CTRL",
+			}),
+		},
+		-- Command+Down Arrow - move to end of line
+		{
+			key = "DownArrow",
+			mods = "CMD",
+			action = wezterm.action.SendKey({
+				key = "e",
+				mods = "CTRL",
+			}),
+		},
 	},
 }
-
 return CONFIG
