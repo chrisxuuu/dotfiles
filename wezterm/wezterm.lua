e9972598f371473e533ddc805361bb7a042cdead
Chris Xu <chris.xu.mps1@gmail.com>
2025-07-06 22:59:03 -0700

    wezterm config edits.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,75 +1,65 @@
 local wezterm = require("wezterm")
 CONFIG = wezterm.config_builder()
 CONFIG = {
-	automatically_reload_config = true,
-	enable_tab_bar = false,
-	window_close_confirmation = "NeverPrompt",
-	window_decorations = "RESIZE",
-	font_size = 14,
-	max_fps = 30,
-	background = {
-		{
-			source = {
-				File = "/Users/chris/Documents/Wallpaper/725002.png",
-			},
-			hsb = {
-				brightness = 0.1,
-			},
-			horizontal_align = "Center",
-		},
-	},
-	keys = {
-		{
-			key = "w",
-			mods = "CMD",
-			action = wezterm.action.CloseCurrentPane({ confirm = true }),
-		},
-		{
-			key = "n",
-			mods = "CMD",
-			action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-		},
-		{
-			key = "n",
-			mods = "CMD|SHIFT",
-			action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-		},
-		-- Command+Left Arrow - move backward one word
-		{
-			key = "LeftArrow",
-			mods = "CMD",
-			action = wezterm.action.SendKey({
-				key = "b",
-				mods = "ALT",
-			}),
-		},
-		-- Command+Right Arrow - move forward one word
-		{
-			key = "RightArrow",
-			mods = "CMD",
-			action = wezterm.action.SendKey({
-				key = "f",
-				mods = "ALT",
-			}),
-		},
-		-- Command+Up Arrow - move to beginning of line
-		{
-			key = "UpArrow",
-			mods = "CMD",
-			action = wezterm.action.SendKey({
-				key = "a",
-				mods = "CTRL",
-			}),
-		},
-		-- Command+Down Arrow - move to end of line
-		{
-			key = "DownArrow",
-			mods = "CMD",
-			action = wezterm.action.SendKey({
-				key = "e",
-				mods = "CTRL",
-			}),
-		},
-	},
+ automatically_reload_config = true,
+ enable_tab_bar = false,
+ window_close_confirmation = "NeverPrompt",
+ window_decorations = "RESIZE",
+ font_size = 14,
+ max_fps = 30,
+ window_background_opacity = 0.25,
+ keys = {
+ {
+ key = "w",
+ mods = "CMD",
+ action = wezterm.action.CloseCurrentPane({ confirm = true }),
+ },
+ {
+ key = "n",
+ mods = "CMD",
+ action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+ },
+ {
+ key = "n",
+ mods = "CMD|SHIFT",
+ action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+ },
+-- Command+Left Arrow - move backward one word
+ {
+ key = "LeftArrow",
+ mods = "CMD",
+ action = wezterm.action.SendKey({
+ key = "b",
+ mods = "ALT",
+ }),
+ },
+-- Command+Right Arrow - move forward one word
+ {
+ key = "RightArrow",
+ mods = "CMD",
+ action = wezterm.action.SendKey({
+ key = "f",
+ mods = "ALT",
+ }),
+ },
+-- Command+Up Arrow - move to beginning of line
+ {
+ key = "UpArrow",
+ mods = "CMD",
+ action = wezterm.action.SendKey({
+ key = "a",
+ mods = "CTRL",
+ }),
+ },
+-- Command+Down Arrow - move to end of line
+ {
+ key = "DownArrow",
+ mods = "CMD",
+ action = wezterm.action.SendKey({
+ key = "e",
+ mods = "CTRL",
+ }),
+ },
+ },
 }
 return CONFIG
