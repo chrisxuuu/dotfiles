348640490bc09e9c63583af5aeebd23e757c0c7c
Chris Xu <chris.xu.mps1@gmail.com>
2025-09-25 14:51:33 -0700

    Updated Wifi Indicator for MacOS 26

--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -5,9 +5,8 @@
 SSID="$(system_profiler SPAirPortDataType | awk '/Current Network/ {getline;$1=$1;print $0 | "tr -d ':'";exit}')"
 #CURR_TX="$(echo "$CURRENT_WIFI" | grep -o "lastTxRate: .*" | sed 's/^lastTxRate: //')"
 
-
 if [ "$SSID" = "" ]; then
-  sketchybar --set $NAME label="Disconnected" icon="󱛅"
+    sketchybar --set $NAME label="󱛅" icon.drawing=off
 else
-  sketchybar --set $NAME label="$SSID" icon=""
+    sketchybar --set $NAME label="" icon.drawing=off
 fi
