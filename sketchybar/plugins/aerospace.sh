0b88d8c9aaf462707a8cac2de8f044a7a4c64b4a
Chris Xu <chris.xu.mps1@gmail.com>
2025-12-03 16:27:25 -0800

    Updated Sketchybar

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -8,14 +8,14 @@ UNFOCUSED_BG="0x601a202c" # Darker muted blue for unfocused workspace
 BORDER_COLOR="0xAA517C9B" # Muted blue border
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-  sketchybar --set $NAME background.color=$FOCUSED_BG \
-    label.shadow.drawing=off \
-    icon.shadow.drawing=off \
-    background.border_width=2 \
-    background.border_color=$BORDER_COLOR
+    sketchybar --set $NAME background.color=$FOCUSED_BG \
+        label.shadow.drawing=off \
+        icon.shadow.drawing=off \
+        background.border_width=0 \
+        background.border_color=$BORDER_COLOR
 else
-  sketchybar --set $NAME background.color=$UNFOCUSED_BG \
-    label.shadow.drawing=off \
-    icon.shadow.drawing=off \
-    background.border_width=0
+    sketchybar --set $NAME background.color=$UNFOCUSED_BG \
+        label.shadow.drawing=off \
+        icon.shadow.drawing=off \
+        background.border_width=0
 fi
